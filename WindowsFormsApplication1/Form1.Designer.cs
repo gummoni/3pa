7c08d2f6326ea346e2fb0e9024175a2a44df3d3a
shaty <shaty@DESKTOP-R10MJOM>
2017-02-27 01:51:52 +0900

    プロジェクト ファイルを追加します。

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,272 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series7 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series8 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series9 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series10 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series11 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series12 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea5 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series13 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series14 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series15 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea6 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series16 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series17 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series18 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.button1 = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart3 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart4 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart5 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart6 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart6)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(984, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 43);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // chart1
+            // 
+            chartArea1.AxisX.Minimum = 0D;
+            chartArea1.AxisX2.Minimum = 0D;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            this.chart1.Location = new System.Drawing.Point(12, 12);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Name = "Series1";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series2.Name = "Series2";
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series3.Name = "Series3";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Series.Add(series3);
+            this.chart1.Size = new System.Drawing.Size(480, 240);
+            this.chart1.TabIndex = 5;
+            this.chart1.Text = "chart1";
+            // 
+            // chart2
+            // 
+            chartArea2.AxisX.Minimum = 0D;
+            chartArea2.AxisX2.Minimum = 0D;
+            chartArea2.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea2);
+            this.chart2.Location = new System.Drawing.Point(498, 12);
+            this.chart2.Name = "chart2";
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series4.Name = "Series1";
+            series5.ChartArea = "ChartArea1";
+            series5.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series5.Name = "Series2";
+            series6.ChartArea = "ChartArea1";
+            series6.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series6.Name = "Series3";
+            this.chart2.Series.Add(series4);
+            this.chart2.Series.Add(series5);
+            this.chart2.Series.Add(series6);
+            this.chart2.Size = new System.Drawing.Size(480, 240);
+            this.chart2.TabIndex = 6;
+            this.chart2.Text = "chart2";
+            // 
+            // chart3
+            // 
+            chartArea3.AxisX.Minimum = 0D;
+            chartArea3.AxisX2.Minimum = 0D;
+            chartArea3.Name = "ChartArea1";
+            this.chart3.ChartAreas.Add(chartArea3);
+            this.chart3.Location = new System.Drawing.Point(12, 258);
+            this.chart3.Name = "chart3";
+            series7.ChartArea = "ChartArea1";
+            series7.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series7.Name = "Series1";
+            series8.ChartArea = "ChartArea1";
+            series8.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series8.Name = "Series2";
+            series9.ChartArea = "ChartArea1";
+            series9.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series9.Name = "Series3";
+            this.chart3.Series.Add(series7);
+            this.chart3.Series.Add(series8);
+            this.chart3.Series.Add(series9);
+            this.chart3.Size = new System.Drawing.Size(480, 240);
+            this.chart3.TabIndex = 7;
+            this.chart3.Text = "chart3";
+            // 
+            // chart4
+            // 
+            chartArea4.AxisX.Minimum = 0D;
+            chartArea4.AxisX2.Minimum = 0D;
+            chartArea4.Name = "ChartArea1";
+            this.chart4.ChartAreas.Add(chartArea4);
+            this.chart4.Location = new System.Drawing.Point(498, 258);
+            this.chart4.Name = "chart4";
+            series10.ChartArea = "ChartArea1";
+            series10.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series10.Name = "Series1";
+            series11.ChartArea = "ChartArea1";
+            series11.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series11.Name = "Series2";
+            series12.ChartArea = "ChartArea1";
+            series12.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series12.Name = "Series3";
+            this.chart4.Series.Add(series10);
+            this.chart4.Series.Add(series11);
+            this.chart4.Series.Add(series12);
+            this.chart4.Size = new System.Drawing.Size(480, 240);
+            this.chart4.TabIndex = 8;
+            this.chart4.Text = "chart4";
+            // 
+            // chart5
+            // 
+            chartArea5.AxisX.Minimum = 0D;
+            chartArea5.AxisX2.Minimum = 0D;
+            chartArea5.Name = "ChartArea1";
+            this.chart5.ChartAreas.Add(chartArea5);
+            this.chart5.Location = new System.Drawing.Point(12, 504);
+            this.chart5.Name = "chart5";
+            series13.ChartArea = "ChartArea1";
+            series13.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series13.Name = "Series1";
+            series14.ChartArea = "ChartArea1";
+            series14.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series14.Name = "Series2";
+            series15.ChartArea = "ChartArea1";
+            series15.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series15.Name = "Series3";
+            this.chart5.Series.Add(series13);
+            this.chart5.Series.Add(series14);
+            this.chart5.Series.Add(series15);
+            this.chart5.Size = new System.Drawing.Size(480, 240);
+            this.chart5.TabIndex = 9;
+            this.chart5.Text = "chart5";
+            // 
+            // chart6
+            // 
+            chartArea6.AxisX.Minimum = 0D;
+            chartArea6.AxisX2.Minimum = 0D;
+            chartArea6.Name = "ChartArea1";
+            this.chart6.ChartAreas.Add(chartArea6);
+            this.chart6.Location = new System.Drawing.Point(498, 504);
+            this.chart6.Name = "chart6";
+            series16.ChartArea = "ChartArea1";
+            series16.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series16.Name = "Series1";
+            series17.ChartArea = "ChartArea1";
+            series17.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series17.Name = "Series2";
+            series18.ChartArea = "ChartArea1";
+            series18.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series18.Name = "Series3";
+            this.chart6.Series.Add(series16);
+            this.chart6.Series.Add(series17);
+            this.chart6.Series.Add(series18);
+            this.chart6.Size = new System.Drawing.Size(480, 240);
+            this.chart6.TabIndex = 10;
+            this.chart6.Text = "chart6";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(984, 61);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(105, 43);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1112, 756);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.chart6);
+            this.Controls.Add(this.chart5);
+            this.Controls.Add(this.chart4);
+            this.Controls.Add(this.chart3);
+            this.Controls.Add(this.chart2);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "U=青, V=黄, W=赤";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart6)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart3;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart4;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart5;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart6;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,283 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        double d, q;
+        double _u, _v, _w;
+        double gain = 100.0;
+        string str = "angle\tu\tv\tw\r\n";
+
+        public Form1()
+        {
+            InitializeComponent();
+            for (var i = 0; i < 360; i++)
+            {
+                var rad = i * Math.PI / 180.0;
+                var a = Math.Cos(rad);
+                var b = Math.Sin(rad);
+
+                var s0 = (i / 60);
+                var s1 = sector(a, b);
+                while (s0 != s1)
+                {
+                    Console.Write("err");
+                    s1 = sector(a, b);
+                }
+
+                var z = Math.Sqrt(3) / 1.5; //115.4%
+            }
+
+            for (var i = 0; i < 360; i++)
+            {
+                var rad = i * Math.PI / 180.0;
+                var a = Math.Cos(rad);
+                var b = Math.Sin(rad);
+
+                var sec = sector(a, b);
+                var rad1 = sec * 60.0 * Math.PI / 180.0;
+                var x = (a * Math.Cos(rad1) + b * Math.Sin(rad1));
+                var y = (b * Math.Cos(rad1) - a * Math.Sin(rad1));
+
+                var A = Math.Cos((i % 60) * Math.PI / 180.0);
+                var B = Math.Sin((i % 60) * Math.PI / 180.0);
+
+                var da = Math.Round(A - x, 4);
+                var db = Math.Round(B - y, 4);
+                if (0 != da || 0 != db)
+                {
+                    Console.Write("");
+                }
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            init();
+            for (var x = 0; x < 360; x++)
+            {
+                d = 1.0; q = 0.0;
+                uvw0(x);
+                chart1.Series[0].Points.AddXY(x, _u);
+                chart1.Series[1].Points.AddXY(x, _v);
+                chart1.Series[2].Points.AddXY(x, _w);
+
+                uvw1(x);
+                chart2.Series[0].Points.AddXY(x, _u);
+                chart2.Series[1].Points.AddXY(x, _v);
+                chart2.Series[2].Points.AddXY(x, _w);
+
+                d = 0.0; q = +1.0;
+                uvw0(x);
+                chart3.Series[0].Points.AddXY(x, _u);
+                chart3.Series[1].Points.AddXY(x, _v);
+                chart3.Series[2].Points.AddXY(x, _w);
+
+                uvw1(x);
+                chart4.Series[0].Points.AddXY(x, _u);
+                chart4.Series[1].Points.AddXY(x, _v);
+                chart4.Series[2].Points.AddXY(x, _w);
+                str += $"{x}\t{_u}\t{_v}\t{_w}\r\n";
+
+                d = 0.0; q = -1.0;
+                uvw0(-x);
+                chart5.Series[0].Points.AddXY(x, _u);
+                chart5.Series[1].Points.AddXY(x, _v);
+                chart5.Series[2].Points.AddXY(x, _w);
+
+                uvw1(-x);
+                chart6.Series[0].Points.AddXY(x, _u);
+                chart6.Series[1].Points.AddXY(x, _v);
+                chart6.Series[2].Points.AddXY(x, _w);
+            }
+            Clipboard.SetText(str);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            init();
+            for (var x = 0; x < 360; x++)
+            {
+                d = 1.0; q = 0.0;
+                uvw0(x);
+                tocurrent();
+                chart1.Series[0].Points.AddXY(x, _u);
+                chart1.Series[1].Points.AddXY(x, _v);
+                chart1.Series[2].Points.AddXY(x, _w);
+
+                uvw1(x);
+                tocurrent();
+                chart2.Series[0].Points.AddXY(x, _u);
+                chart2.Series[1].Points.AddXY(x, _v);
+                chart2.Series[2].Points.AddXY(x, _w);
+
+                d = 0.0; q = +1.0;
+                uvw0(x);
+                tocurrent();
+                chart3.Series[0].Points.AddXY(x, _u);
+                chart3.Series[1].Points.AddXY(x, _v);
+                chart3.Series[2].Points.AddXY(x, _w);
+
+                uvw1(x);
+                tocurrent();
+                chart4.Series[0].Points.AddXY(x, _u);
+                chart4.Series[1].Points.AddXY(x, _v);
+                chart4.Series[2].Points.AddXY(x, _w);
+                str += $"{x}\t{_u}\t{_v}\t{_w}\r\n";
+
+                d = 0.0; q = -1.0;
+                uvw0(-x);
+                tocurrent();
+                chart5.Series[0].Points.AddXY(x, _u);
+                chart5.Series[1].Points.AddXY(x, _v);
+                chart5.Series[2].Points.AddXY(x, _w);
+
+                uvw1(-x);
+                tocurrent();
+                chart6.Series[0].Points.AddXY(x, _u);
+                chart6.Series[1].Points.AddXY(x, _v);
+                chart6.Series[2].Points.AddXY(x, _w);
+            }
+            Clipboard.SetText(str);
+        }
+        void uvw0(double angle)
+        {
+            var rad = angle * Math.PI / 180.0;
+#if true
+#if false
+            //つっかかるような反転(現在)
+            var a = +d * Math.Cos(rad) + q * Math.Sin(rad);
+            var b = -d * Math.Sin(rad) + q * Math.Cos(rad);
+#else
+            //ちゃんとした反転
+            var a = +d * Math.Cos(rad) - q * Math.Sin(rad);
+            var b = -d * Math.Sin(rad) - q * Math.Cos(rad);
+#endif
+#else
+            //合わせた波形(正転)　
+            var a = +d * Math.Cos(rad) - q * Math.Sin(rad);
+            var b = +d * Math.Sin(rad) + q * Math.Cos(rad);
+#endif
+            var u = a;
+            var v = (Math.Sqrt(3) * b - a) / 2;
+            var w = -(u + v);
+            _u = (1.0 - u) * gain / 2;
+            _v = (1.0 - v) * gain / 2;
+            _w = (1.0 - w) * gain / 2;
+        }
+        void uvw1(int angle)
+        {
+            var deg = angle * Math.PI / 180.0;
+#if true
+#if false
+            //つっかかるような反転
+            var a0 = (+d * Math.Cos(deg) + q * Math.Sin(deg));
+            var b0 = (-d * Math.Sin(deg) + q * Math.Cos(deg));
+#else
+            //反転
+            var a0 = (+d * Math.Cos(deg) - q * Math.Sin(deg));
+            var b0 = (-d * Math.Sin(deg) - q * Math.Cos(deg));
+#endif
+#else
+            //正転
+            var a0 = (+d * Math.Cos(deg) - q * Math.Sin(deg));
+            var b0 = (+d * Math.Sin(deg) + q * Math.Cos(deg));
+#endif
+            var sec = sector(a0, b0);
+            var rad0 = sec * 60 * Math.PI / 180.0;
+            var a = gain * ((a0 * Math.Cos(rad0) + b0 * Math.Sin(rad0)));        // cosA*cosB + sinA*sinB
+            var b = gain * ((b0 * Math.Cos(rad0) - a0 * Math.Sin(rad0)));        // sinA*cosB - cosA*sinB
+
+            var t1 = (Math.Sqrt(3) * a - b) / 2.0f;
+            var t2 = b;
+            var t3 = gain - (t1 + t2);
+            var t3a = t3 / 2.0f;
+            var t23a = t2 + t3a;
+            var t123a = t1 + t23a;
+            var t3b = gain - t3a;
+            var t23b = gain - t23a;
+            var t123b = gain - t123a;
+
+            //反転のときつっかかるような反転
+            if (2 == sec)      { _u = t123a; _w = t23b; _v = t3a; }
+            else if (3 == sec) { _w = t123b; _v = t23a; _u = t3b; }
+            else if (4 == sec) { _v = t123a; _u = t23b; _w = t3a; }
+            else if (5 == sec) { _u = t123b; _w = t23a; _v = t3b; }
+            else if (0 == sec) { _w = t123a; _v = t23b; _u = t3a; }
+            else               { _v = t123b; _u = t23a; _w = t3b; }
+        }
+        void tocurrent()
+        {
+            int u = 0, v = 0, w = 0;
+            for (var i = 0; i < gain * 2; i++)
+            {
+                var bu = (_u <= i);
+                var bv = (_v <= i);
+                var bw = (_w <= i);
+
+                if (!bu && !bv && !bw) { u -= 0; v -= 0; w -= 0; }
+                if (!bu && !bv &&  bw) { u -= 1; v -= 1; w += 2; }
+                if (!bu &&  bv && !bw) { u -= 1; v += 2; w -= 1; }
+                if (!bu &&  bv &&  bw) { u -= 2; v += 1; w += 1; }
+                if ( bu && !bv && !bw) { u += 2; v -= 1; w -= 1; }
+                if ( bu && !bv &&  bw) { u += 1; v -= 2; w += 1; }
+                if ( bu &&  bv && !bw) { u += 1; v += 1; w -= 2; }
+                if ( bu &&  bv &&  bw) { u += 0; v += 0; w += 0; }
+            }
+            _u = u;
+            _v = v;
+            _w = w;
+        }
+
+        int sector(double x, double y)
+        {
+            x = Math.Round(x, 3);
+            y = Math.Round(y, 3);
+            if (0 <= x)
+            {
+                if (0 <= y)
+                    return (0.5 >= x) ? 1 : 0;
+                else
+                    return (0.5 <= x) ? 5 : 4;
+            }
+            else
+            {
+                if (0 < y)
+                    return (-0.5 >= x) ? 2 : 1;
+                else
+                    return (-0.5 <= x) ? 4 : 3;
+            }
+        }
+
+        void init()
+        {
+            chart1.Series[0].Points.Clear();
+            chart1.Series[1].Points.Clear();
+            chart1.Series[2].Points.Clear();
+            chart2.Series[0].Points.Clear();
+            chart2.Series[1].Points.Clear();
+            chart2.Series[2].Points.Clear();
+            chart3.Series[0].Points.Clear();
+            chart3.Series[1].Points.Clear();
+            chart3.Series[2].Points.Clear();
+            chart4.Series[0].Points.Clear();
+            chart4.Series[1].Points.Clear();
+            chart4.Series[2].Points.Clear();
+            chart5.Series[0].Points.Clear();
+            chart5.Series[1].Points.Clear();
+            chart5.Series[2].Points.Clear();
+            chart6.Series[0].Points.Clear();
+            chart6.Series[1].Points.Clear();
+            chart6.Series[2].Points.Clear();
+            str = "angle\tu\tv\tw\r\n";
+        }
+    }
+}
